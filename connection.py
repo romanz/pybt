169f63f715fa66d319157737a2a5da723b43b29a
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-12 10:51:05 +0300

    Move protocol into separate file.

--- connection.py
+++ connection.py
@@ -1,5 +1,7 @@
+from gevent import socket
+
 class Connection:
-    def __init__(self, socket, addr, timeout=None):
+    def __init__(self, addr, timeout=None):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if timeout is not None:
             sock.settimeout(timeout)
