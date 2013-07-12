51ac715ccd551821b48d584ca10a6c2e297c91e3
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-12 14:55:41 +0300

    Connection logging.

--- connection.py
+++ connection.py
@@ -1,11 +1,16 @@
+import logging
+
 from gevent import socket
+
+log = logging.getLogger('connection')
 
 class Connection:
     def __init__(self, addr, timeout=None):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if timeout is not None:
             sock.settimeout(timeout)
-        sock.connect(addr)
+        log.info('connecting to peer at {}'.format(addr))
+        sock.connect(addr)        
         self.sock = sock
 
     def recv(self, n):        
@@ -14,7 +19,8 @@ class Connection:
             left = n - len(data)
             if left == 0:
                 return data
-            data = data + self.sock.recv(left)
+            buf = self.sock.recv(left)
+            data = data + buf
 
     def send(self, data):
         self.sock.sendall(data)
