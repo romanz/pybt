25645708e977487f613dbf2c2d1a4a074e1a2dd4
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-13 18:11:33 +0300

    Raise connection.Closed() when socket fails.

--- connection.py
+++ connection.py
@@ -7,9 +7,6 @@ log = logging.getLogger('connection')
 class Closed(Exception):
     pass
 
-class Timeout(Exception):
-    pass
-
 class Connection:
     def __init__(self, addr, timeout=None):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,10 +15,8 @@ class Connection:
         log.info('connecting to peer at {}'.format(addr))
         try:
             sock.connect(addr)        
-        except socket.timeout:
-            raise Timeout()
-        except socket.error:
-            raise Closed()
+        except (socket.timeout, socket.error) as e:
+            raise Closed(e)
         self.sock = sock
 
     def recv(self, n):        
@@ -33,15 +28,16 @@ class Connection:
 
             try:
                 buf = self.sock.recv(left)
-            except socket.timeout:
-                raise Timeout()
-            except socket.error:
-                raise Closed()
+            except (socket.timeout, socket.error) as e:
+                raise Closed(e)
 
             if not buf: # peer socket is closed
-                raise Closed()
+                raise Closed('peer closed connection')
 
             data = data + buf
 
     def send(self, data):
-        self.sock.sendall(data)
+        try:
+            self.sock.sendall(data)
+        except (socket.timeout, socket.error) as e:
+            raise Closed(e)
--- test.py
+++ test.py
@@ -20,19 +20,14 @@ def main(args):
 
     if any(~data.bits):
         host_id = metainfo.hash(args.host_id)
-        peers = tracker.get_peers(meta, host_id, port=6889)
+        peers = tracker.get_peers(meta, host_id, port=6889, timeout=10)
         for addr in peers:
             try:
                 conn = connection.Connection(addr, timeout=10)
                 p = protocol.PeerProtocol(host_id, data, conn=conn)
                 p.loop()
-            except connection.Closed:
-                log.warning('connection closed by peer')
-            except connection.Timeout:
-                log.warning('peer timeout')
-
-    log.info('Download is over')
-    return
+            except connection.Closed as e:
+                log.warning('{} connection closed: {}'.format(addr, e))
 
 if __name__ == '__main__':
     import argparse
