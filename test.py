c506248dbea9d3ee308d4d370187ccc2a5d84fdf
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-14 10:19:05 +0300

    Handle several peers concurrently using gevent.spawn

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ import gevent
 
 log = logging.getLogger('main')
 
+def handle_peer(host_id, data, addr):
+    try:
+        conn = connection.Connection(addr, timeout=60)
+        p = protocol.PeerProtocol(host_id, data, conn=conn)
+        p.loop()
+    except connection.Closed as e:
+        log.warning('{} connection closed: {}'.format(addr, e))
+
 def main(args):
     logging.basicConfig(
         format='%(asctime)-15s [%(levelname)s] %(message)s', 
@@ -20,14 +28,9 @@ def main(args):
 
     if any(~data.bits):
         host_id = metainfo.hash(args.host_id)
-        peers = tracker.get_peers(meta, host_id, port=6889, timeout=10)
-        for addr in peers:
-            try:
-                conn = connection.Connection(addr, timeout=10)
-                p = protocol.PeerProtocol(host_id, data, conn=conn)
-                p.loop()
-            except connection.Closed as e:
-                log.warning('{} connection closed: {}'.format(addr, e))
+        peers = tracker.get_peers(meta, host_id, port=6889, timeout=10, num_want=50)
+        jobs = [gevent.spawn(handle_peer, host_id, data, addr) for addr in peers]
+        gevent.joinall(jobs)
 
 if __name__ == '__main__':
     import argparse
