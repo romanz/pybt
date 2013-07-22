189d65cf687bc810c7fbe1c780190c813ca0447c
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-22 20:53:41 +0300

    Refactor tracker code.

--- tracker.py
+++ tracker.py
@@ -18,6 +18,73 @@ MAX_PACKET_SIZE = 2**16
 
 log = logging.getLogger('tracker')
 
+INIT_ID = 0x41727101980
+
+connect_req = c.Struct('request', 
+    c.UBInt64('connection_id'),
+    c.UBInt32('action'),
+    c.UBInt32('transaction_id')
+)
+
+connect_resp = c.Struct('response',
+    c.UBInt32('action'),
+    c.UBInt32('transaction_id'),
+    c.UBInt64('connection_id'),
+)
+
+announce_req = c.Struct('request',
+    c.UBInt64('connection_id'),
+    c.UBInt32('action'),
+    c.UBInt32('transaction_id'),
+    c.Bytes('info_hash', 20),
+    c.Bytes('peer_id', 20),
+    c.UBInt64('downloaded'),
+    c.UBInt64('left'),
+    c.UBInt64('uploaded'),
+    c.UBInt32('event'),
+    c.UBInt32('ip_addr'),
+    c.UBInt32('key'),
+    c.SBInt32('num_want'),
+    c.UBInt16('port'),
+)
+
+announce_resp = c.Struct('response', 
+    c.UBInt32('action'), 
+    c.UBInt32('transaction_id'),
+    c.UBInt32('interval'),
+    c.UBInt32('leechers'),
+    c.UBInt32('seeders'),
+    c.GreedyRange(
+        c.Struct('peer',
+            c.Array(4, c.UBInt8('addr')),
+            c.UBInt16('port')
+        )
+    )
+)
+
+scrape_req = c.Struct('request', 
+    c.UBInt64('connection_id'),
+    c.UBInt32('action'),
+    c.UBInt32('transaction_id'),
+    c.GreedyRange(
+        c.Struct('hashes',
+            c.Bytes('info_hash', 20),
+        )
+    )
+)
+
+scrape_resp = c.Struct('response',
+    c.UBInt32('action'),
+    c.UBInt32('transaction_id'),
+    c.GreedyRange(
+        c.Struct('stats',
+            c.UBInt32('seeders'),
+            c.UBInt32('completed'),
+            c.UBInt32('leechers'),
+        )
+    )
+)
+
 class udp:
     def __init__(self, addr, timeout=None):
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,14 +95,9 @@ class udp:
         self.id = None
 
     def connect(self):
-        req = c.Struct('request', 
-                c.UBInt64('connection_id'),
-                c.UBInt32('action'),
-                c.UBInt32('transaction_id')
-            )
         tx = random.getrandbits(32)
-        obj = c.Container(connection_id=0x41727101980, action=0, transaction_id=tx)
-        msg = req.build(obj)
+        obj = c.Container(connection_id=INIT_ID, action=0, transaction_id=tx)
+        msg = connect_req.build(obj)
 
         log.debug('transaction: {}'.format(tx))
         self.conn.send(msg)
@@ -43,13 +105,7 @@ class udp:
         # handle response
         msg = self.conn.recv(MAX_PACKET_SIZE)
 
-        resp = c.Struct('response',
-                c.UBInt32('action'),
-                c.UBInt32('transaction_id'),
-                c.UBInt64('connection_id'),
-            )
-
-        obj = resp.parse(msg)
+        obj = connect_resp.parse(msg)
         log.debug('connection: {}'.format(obj.connection_id))
 
         if obj.action != 0:
@@ -65,22 +121,6 @@ class udp:
         if self.conn is None:
             raise Error('Must connect to tracker before announcement')
 
-        req = c.Struct('request',
-                c.UBInt64('connection_id'),
-                c.UBInt32('action'),
-                c.UBInt32('transaction_id'),
-                c.Bytes('info_hash', 20),
-                c.Bytes('peer_id', 20),
-                c.UBInt64('downloaded'),
-                c.UBInt64('left'),
-                c.UBInt64('uploaded'),
-                c.UBInt32('event'),
-                c.UBInt32('ip_addr'),
-                c.UBInt32('key'),
-                c.SBInt32('num_want'),
-                c.UBInt16('port'),
-            )
-
         tx = random.getrandbits(32)
         kw = {
             'transaction_id': tx, 
@@ -95,31 +135,13 @@ class udp:
         kw.update(data)
         obj = c.Container(**kw)
 
-        msg = req.build(obj)
+        msg = announce_req.build(obj)
         log.debug('request peers for {}'.format(binascii.hexlify(obj.info_hash)))
         self.conn.send(msg)
 
         # handle response
         msg = self.conn.recv(MAX_PACKET_SIZE)        
-        
-        if msg[0] != '\x00':
-            raise Error('Invalid response: {}'.format(repr(msg)))
-
-        resp = c.Struct('response', 
-            c.UBInt32('action'), 
-            c.UBInt32('transaction_id'),
-            c.UBInt32('interval'),
-            c.UBInt32('leechers'),
-            c.UBInt32('seeders'),
-            c.GreedyRange(
-                c.Struct('peer',
-                    c.Array(4, c.UBInt8('addr')),
-                    c.UBInt16('port')
-                )
-            )
-        )
-
-        obj = resp.parse(msg)
+        obj = announce_resp.parse(msg)
 
         if obj.action != 1:
             raise Error('Incorrect action: {}'.format(obj))
@@ -170,7 +192,7 @@ def test_tracker(fname):
         format='%(asctime)-15s [%(levelname)s] %(message)s', 
         level=logging.DEBUG)
     meta = metainfo.MetaInfo(fname)
-    peers = get_peers(meta, peer_id=metainfo.hash('test_tracker'), port=6889)
+    peers = get_peers(meta, peer_id=metainfo.hash('test_tracker'), port=6889, timeout=10)
     print peers
 
 if __name__ == '__main__':
