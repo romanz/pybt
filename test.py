fe6e968e33e1f2c1d8c38d01d72b2c46095f88fd
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-22 14:50:43 +0300

    Add pool for limiting concurrent connections.

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import storage
 import tracker
 
 import gevent
+import gevent.pool
 
 log = logging.getLogger('main')
 
@@ -26,15 +27,19 @@ def main(args):
     meta = metainfo.MetaInfo(args.filename)
     data = storage.Data(meta)
 
-    if any(~data.bits):
-        host_id = metainfo.hash(args.host_id)
-        peers = tracker.get_peers(meta, host_id, port=6889, timeout=10, num_want=50)
-        jobs = [gevent.spawn(handle_peer, host_id, data, addr) for addr in peers]
-        gevent.joinall(jobs)
+    host_id = metainfo.hash(args.id)            
+    pool = gevent.pool.Pool(args.pool)
+    while any(~data.bits):
+        peers = tracker.get_peers(meta, host_id, port=6889, timeout=10, num_want=-1)
+        if peers:
+            for addr in peers:
+                pool.spawn(handle_peer, host_id, data, addr)
+            pool.join()
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Python BitTorrent Client')
     parser.add_argument('filename')
-    parser.add_argument('host_id')
+    parser.add_argument('--id')
+    parser.add_argument('--pool', default=1, type=int)
     main(parser.parse_args())
