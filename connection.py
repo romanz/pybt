aa009688acc04f1a7c65a1ec06a1795db11f87d2
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-22 14:50:17 +0300

    Add simple throttling to connection.

--- connection.py
+++ connection.py
@@ -1,5 +1,7 @@
 import logging
+import functools
 
+import gevent
 from gevent import socket
 
 log = logging.getLogger('connection')
@@ -8,7 +10,7 @@ class Closed(Exception):
     pass
 
 class Connection:
-    def __init__(self, addr, timeout=None):
+    def __init__(self, addr, timeout=None, rx=None, tx=None):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if timeout is not None:
             sock.settimeout(timeout)
@@ -18,26 +20,53 @@ class Connection:
         except (socket.timeout, socket.error) as e:
             raise Closed(e)
         self.sock = sock
+        self.rx = _create_throttle(rx)
+        self.tx = _create_throttle(tx)
 
     def recv(self, n):        
+        try:
+            data = _recvall(self.sock, n)
+        except (socket.timeout, socket.error) as e:
+            raise Closed(e)
+
+        if data is None: # peer socket is closed
+            raise Closed('peer closed connection')
+
+        return self.rx(data) # throttle RX bandwidth
+
+    def send(self, data):
+        try:
+            data = self.tx(data) # throttle TX bandwidth
+            self.sock.sendall(data)
+        except (socket.timeout, socket.error) as e:
+            raise Closed(e)
+
+def _recvall(sock, n):
         data = ''
         while True:
             left = n - len(data)
             if left == 0:
                 return data
 
-            try:
-                buf = self.sock.recv(left)
-            except (socket.timeout, socket.error) as e:
-                raise Closed(e)
-
+            buf = sock.recv(left)
             if not buf: # peer socket is closed
-                raise Closed('peer closed connection')
+                return None
 
             data = data + buf
 
-    def send(self, data):
-        try:
-            self.sock.sendall(data)
-        except (socket.timeout, socket.error) as e:
-            raise Closed(e)
+def _create_throttle(obj):
+
+    if obj is None:
+        return (lambda x: x) # no-op function
+
+    if isinstance(obj, (int, float, long)):
+        bw = float(obj)
+        return functools.partial(throttle, bandwidth=bw)
+
+    return obj # return object as is
+
+def throttle(data, bandwidth=None):
+     # time needed for data to be sent/received
+    dt = (len(data) / bandwidth) if bandwidth else 0.0
+    gevent.sleep(dt)
+    return data
