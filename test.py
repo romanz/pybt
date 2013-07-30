14c1bb5db9016a6e64b6fa15c712fd9fe2101765
Roman Zeyde <roman.zeyde@gmail.com>
2013-07-30 14:33:35 +0300

    Add commandline options to test.py

--- test.py
+++ test.py
@@ -12,23 +12,27 @@ def main(args):
 
     host_id = metainfo.hash(args.host)
 
-    link = 'magnet:?xt=urn:btih:e1dfffe75594e5541f4e8bfd9245f20c1beb1003&dn=Adventure.Time.S05E27.HDTV.x264-W4F.mp4&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337'
-    m = metainfo.parse_magnet(link)
-
+    m = metainfo.parse_magnet(args.link)
     meta = download.metadata_load(m['info_hash'])
-    if meta is None:
-        dl = download.Metadata(host_id, m['info_hash'])
-        meta = dl.run(m['trackers'])
 
-    dl = download.Torrent(host_id, meta)
-    if not all(dl.data.bits):
-        dl.run(m['trackers'])
-    log.info('Download completed')
+    if args.metadata:
+        if meta is None:
+            dl = download.Metadata(host_id, m['info_hash'])
+            meta = dl.run(m['trackers'])
+
+    if args.torrent and meta:
+        dl = download.Torrent(host_id, meta)
+        if not all(dl.data.bits):
+            dl.run(m['trackers'])
+        log.info('Download completed')
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Simple Python BitTorrent Client')
-    parser.add_argument('--host', default='N/A')
+    parser.add_argument('--host', default='none')
+    parser.add_argument('--link', default='')
+    parser.add_argument('--metadata', action='store_true', default=False)
+    parser.add_argument('--torrent', action='store_true', default=False)
     args = parser.parse_args()
 
     try:
